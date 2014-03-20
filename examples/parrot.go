9335ec9fc5aef913e5d34223f2096b34d68f9d26
Gleicon Moraes <gleicon@gmail.com>
2014-03-20 18:08:47 -0300

    WIP

--- examples/parrot.go
+++ examples/parrot.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/gleicon/swarm"
-	"testing"
 )
 
 var cluster = []string{"10.0.0.1"}
-var myee = NewDistributedEventEmitter(cluster, "10.0.0.1")
+var myee = swarm.NewDistributedEventEmitter(cluster, "10.0.0.1")
 
 func main() {
 	myee.On("parrot", func(w []byte) {
